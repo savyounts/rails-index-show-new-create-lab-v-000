8c37253619f7edc58732d4bb7a9920e8adc8e78d
Savannah Younts <savyounts@gmail.com>
2018-09-19 19:28:18 +0000

    automatically backed up by learn

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -1,14 +1,18 @@
 class CouponController < ApplicationController
 
   def index
+    @coupons = Coupon.all
   end
 
   def show
+    @coupon = Coupon.find(params[:id])
   end
 
   def new
   end
 
   def create
+      @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+      redirect_to coupon_path
   end
 end
